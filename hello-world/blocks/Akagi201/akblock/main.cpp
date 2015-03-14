e02fba7ee2bdfba535803d4a415322a116b0830b
Akagi201 <akagi201@gmail.com>
2015-03-14 16:02:06 +0800

    add hello-world

---
+++ hello-world/blocks/Akagi201/akblock/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * Print a simple "Hello world!"
+ *
+ * @file main.cpp
+ * @section LICENSE
+
+    This code is under MIT License, http://opensource.org/licenses/MIT
+ */
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello world!\n";
+}
