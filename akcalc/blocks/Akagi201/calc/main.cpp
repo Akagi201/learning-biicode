6450233054b93d1c6087964362acad69dfe59966
Akagi201 <akagi201@gmail.com>
2015-03-16 13:44:42 +0800

    add calc

---
+++ akcalc/blocks/Akagi201/calc/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * Print a simple "Hello world!"
+ *
+ * @file main.cpp
+ * @section LICENSE
+
+    This code is under MIT License, http://opensource.org/licenses/MIT
+ */
+
+ #include <iostream>
+ #include "Akagi201/math/operations.h" //NOTE: Replace myuser!
+
+ using namespace std;
+ int main() {
+    cout<<"2 + 3 = "<< sum(2, 3)<<endl;
+ }
